2d1ad25299f37e4c7961f469cab65ee06542ac83
qqu0127 <qqu0127@gmail.com>
2018-08-09 15:18:49 -0400

    make interactive

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ var colors = ["rgb(255, 0, 0)",
 var colorDisplay = document.querySelector("#colorDisplay");
 var squares = document.querySelectorAll(".square");
 var clickedColor = 0;
-var pickedColor = colors[5];
+var colorInd = getRandomInt(6);
+var pickedColor = colors[colorInd];
 
 for(var i = 0; i < squares.length; i++){
 	//set initial color
@@ -20,11 +21,26 @@ for(var i = 0; i < squares.length; i++){
 		//colorDisplay.textContent = this.style.backgroundColor;
 		clickedColor = this.style.backgroundColor; 
 
-		if(pickedColor === clickedColor)
+		if(pickedColor === clickedColor){
 			alert("You are right");
+			colorInd = getRandomInt(6);
+			resetGame();
+			pickedColor = colors[colorInd];
+			colorDisplay.textContent = pickedColor;
+		}
 		else
-			alert("Tye again");
+			this.style.backgroundColor = "#232323";
 	});
 }
 colorDisplay.textContent = pickedColor;
-colorDisplay.style.color = pickedColor;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function resetGame(){
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+
+	}
+}
